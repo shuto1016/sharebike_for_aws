deea2cc21ba8d54f3a3389df0112c2e46e75ae20
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-16 08:00:45 +0900

    Alert.js追加

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,12 +1,3 @@
-
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-//= require jquery
-//= require jquery_ujs
-//= require alert.js
-
 $(function(){
   $(".delete").click(function(){
     var result = confirm('本当に削除しますか？');
