2ab17c807ad2c099cb9e3f58dc875b5409471f54
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-07-22 07:38:44 +0900

    userとarticleのseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  [
+    {
+      email: 'test@test.com',
+      password: '111111',
+      nickname: 'テスト太郎',
+    },
+    {
+      email: 'test2@test.com',
+      password: '111111',
+      nickname: 'テスト太郎2',
+    }
+  ]
+
+)
+
+Article.create!(
+  [
+    {
+      user_id: '1',
+      category_id: '1',
+      title: 'アイウエオ',
+      text: '今日は暑い'
+    },
+    {
+      user_id: '2',
+      category_id: '2',
+      title: 'かくけこ',
+      text: '今日は寒い'
+    }
+  ]
+
+)
