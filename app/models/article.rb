a408c9c9532d9777a27f3d5fcb92e5f046249125
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-11 16:44:46 +0900

    Imagesとのアソシエーションにdependent: :destroy追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
   validates :text, presence: true
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
   
 end
