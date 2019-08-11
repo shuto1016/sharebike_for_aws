faaf52f580206ba58893da50631292fa54de6da2
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-11 09:18:11 +0900

    Imageモデル作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :images
   validates :text, presence: true
-
+  accepts_nested_attributes_for :images
+  
 end
---
+++ app/models/image.rb
@@ -0,0 +1,5 @@
+class Image < ApplicationRecord
+  belongs_to :article
+  mount_uploader :image, ImageUploader
+
+end
