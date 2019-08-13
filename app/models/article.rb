80e9b7d8b35a1dd50658c6b0614754344bb68c17
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-13 17:28:46 +0900

    Liked?メソッド定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,5 +7,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
 
   accepts_nested_attributes_for :images, allow_destroy: true
-  
+
+  def liked? (liked_user_id, liked_article_id)
+    likes.where(user_id: liked_user_id, article_id: liked_article_id).exists?
+   end  
 end
