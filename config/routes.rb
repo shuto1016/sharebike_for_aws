dcae024851e9edb66e0c85956c95ff7f32c277e3
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-13 17:29:03 +0900

    routeパス定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'blogs#index'
-  resources :blogs do
-    resources :likes, only: [:create, :destroy]
-  end
+  resources :blogs 
+  
+  post   '/like/:article_id' => 'like#create',   as: 'like'
+  delete '/like/:id' => 'like#destroy', as: 'unlike'
 end
