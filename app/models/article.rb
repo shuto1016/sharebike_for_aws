59850ff34d6419f758c089d7be2abb6c95f10ff7
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-13 12:17:10 +0900

    Likeアソシエーション追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
   validates :text, presence: true
+  validates :title, presence: true
+
   accepts_nested_attributes_for :images, allow_destroy: true
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
+  has_many :likes, dependent: :destroy
+  
   validates :nickname, uniqueness: true, length: {maximum: 8 }
 end
