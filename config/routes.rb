f09b4fe912a49cb401031139712f896974328f6a
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-13 12:18:03 +0900

    Like route 追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'blogs#index'
-  resources :blogs
+  resources :blogs do
+    resources :likes, only: [:create, :destroy]
+  end
 end
