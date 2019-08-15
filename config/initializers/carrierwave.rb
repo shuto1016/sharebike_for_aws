46e89568ad40a9ca4ee75dd0cb30f355480aacc7
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-15 09:12:26 +0900

    ''から""に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
