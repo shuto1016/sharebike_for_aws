f3bd534d77f6a9a796feac47973e9e23a2a173b8
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-15 20:20:48 +0900

    不要なセンテンス削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
 
   factory :user do
-    # nickname              {"test"}
     password              {"00000000"}
     password_confirmation {"00000000"}
     sequence(:nickname) {Faker::Internet.username(specifier: 5..8)
