653d1dde61aab030df7939b43af0cdb626426132
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-15 22:08:02 +0900

    blog_controller_spec追記

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -1,38 +1,71 @@
 require 'rails_helper'
 
 describe BlogsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:article) { create(:article, user: user) }
+
 
   describe 'GET #new' do
-    it "#newが期待するビューに遷移すること" do
-      get :new
-      expect(response).to render_template :new
+    context 'ログイン時' do
+      before do
+        login user
+        get :new
+      end
+
+      it "#newが期待するビューに遷移すること" do
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'ログアウト時' do
+      before do
+        get :new
+      end
+
+      it "#newがログイン画面にリダイレクトすること" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 
   describe 'GET #edit' do
-    it "#editの@articleは期待した値になること" do
-      article = create(:article)
-      get :edit, params: { id: article }
-      expect(assigns(:article)).to eq article
+    context 'ログイン時' do
+      before do
+        login user
+        
+        get :edit, params: { id: article }
+      end
+
+      it "#editの@articleは期待した値になること" do
+        expect(assigns(:article)).to eq article
+      end
+
+      it "#editが期待するビューに遷移すること" do
+        expect(response).to render_template :edit
+      end
     end
 
-    it "#editが期待するビューに遷移すること" do
-    article = create(:article)
-    get :edit, params: { id: article }
-    expect(response).to render_template :edit
+    context 'ログアウト時' do
+      before do
+        get :edit, params: { id: article }
+      end
+
+      it "#editがログイン画面にリダイレクトすること" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 
   describe 'GET #index' do
     it "#indexの@articlesは期待した配列になり、作成順で取得できること" do
-    articles = create_list(:article, 3) 
-    get :index
-    expect(assigns(:articles)).to match(articles.sort{ |a, b| a.created_at <=> b.created_at })
-
+      articles = create_list(:article, 3) 
+      get :index
+      expect(assigns(:articles)).to match(articles.sort{ |a, b| b.id <=> a.id })
     end
+
     it "#indexが期待するビューに遷移すること" do
-    get :index
-    expect(response).to render_template :index
+      get :index
+      expect(response).to render_template :index
     end
   end
 
