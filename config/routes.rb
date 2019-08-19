b52c7f06e7b9d87f4c99c06e3efcfd2ff3569466
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-19 18:44:54 +0900

    Comment のルートパス追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'blogs#index'
-  resources :blogs 
+  resources :blogs do
+    resources :comments, only: [:create]
+  end
+  
   resources :like, only: [:index]
   post   '/like/:article_id' => 'like#create',   as: 'like'
   delete '/like/:id' => 'like#destroy', as: 'unlike'
