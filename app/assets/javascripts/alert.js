739596cbacc962f2f49143d1cc9e9fa49592ed4e
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-21 21:11:24 +0900

    Alert.jsを別ファイルに

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".delete").click(function(){
+    var result = confirm('本当に削除しますか？');
+    if(result){
+    } else{
+      return false;
+    }
+  });
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,13 +7,4 @@
 //= require_tree .
 //= require alert.js
 
-$(function(){
-  $(".delete").click(function(){
-    var result = confirm('本当に削除しますか？');
-    if(result){
-    } else{
-      return false;
-    }
-  });
-});
 
