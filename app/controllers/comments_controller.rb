5acd456a6c274ec13f59c8bb6994399e75fd79c7
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-08-23 22:14:38 +0900

    comments#deleteアクションを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:comment_id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      respond_to do |format|
+        format.html {redirect_to blog_path(@comment.article_id)}
+        format.json{ render json: @comment}
+      end
+    end
+  end
+
   private
   def comment_params
     params.permit(:article_id, :text).merge(user_id: current_user.id)
