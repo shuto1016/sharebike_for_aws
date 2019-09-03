a4702579aed919f789e8d7ae5f96b580533693b3
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-09-04 00:58:19 +0900

    maker_seedファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_121618) do
+ActiveRecord::Schema.define(version: 2019_08_29_101356) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.integer "category_id"
-    t.string "title", default: "", null: false
+    t.string "title", null: false
     t.text "text", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["title"], name: "index_articles_on_title"
+  end
+
+  create_table "articles_makers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "maker_id"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["title"], name: "index_articles_on_title"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -41,12 +47,18 @@ ActiveRecord::Schema.define(version: 2019_08_18_121618) do
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "article_id", null: false
+    t.bigint "article_id"
     t.bigint "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_likes_on_article_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "makers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
