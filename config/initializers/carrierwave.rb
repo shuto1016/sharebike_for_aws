09c1f0394066cd5c0970cebcd662e941c94ba436
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-09-08 17:04:55 +0900

    Fog/awsに修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog'
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
